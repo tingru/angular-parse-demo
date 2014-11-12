6641f377482f43f2e9e16587d954d3f10192bfde
tingru <guotingru@outlook.com>
2014-11-12 01:21:13 -0800

    change1

--- js/github-app.js
+++ js/github-app.js
@@ -7,26 +7,46 @@
  */
 
 
+//angular.module('GitHubApp', [])
+//    .controller('GitHubController', function($scope, $http) {
+//        $scope.userName = 'tingru';
+//        $scope.getRepos = function() {
+
+//            $http.get('https://api.github.com/users/' + $scope.userName + '/repos')
+//                .success(function(data) {
+//                    $scope.repos = data;
+//                    $scope.errorMessage = null;
+//                    $scope.loading = false;
+//            })
+//                .error(function(err){
+//                   //alert('User does not exist!');
+//                    $scope.errorMessage = err.message || 'User does not exist!';
+//                    $scope.loading = false;
+//                })
+//            .finally(function() {
+//                    $scope.loading = false;
+//                });
+
+
+//        };
+//    });
+
 angular.module('GitHubApp', [])
-    .controller('GitHubController', function($scope, $http) {
-        $scope.userName = 'drstearns';
-        $scope.getRepos = function() {
+    .controller('GitHubController', function ($scope, $http) {
+        $scope.userName = 'tingru';
+        $scope.getRepos = function () {
+            $scope.loading = true
+            $http.get('https://api.github.com/users/' + $scope.userName + '/repos').success(function (data) {
+                $scope.repos = data;
+                $scope.errorMessage = null;
+            }).error(function (err) {
+                $scope.errorMessage = err.message;
+                //                alert('user does not exist');
+            }).finally(function () {
+                $scope.loading = false;
 
-            $http.get('https://api.github.com/users/' + $scope.userName + '/repos')
-                .success(function(data) {
-                    $scope.repos = data;
-                    $scope.errorMessage = null;
-                    $scope.loading = false;
-            })
-                .error(function(err){
-                   //alert('User does not exist!');
-                    $scope.errorMessage = err.message || 'User does not exist!';
-                    $scope.loading = false;
-                })
-            .finally(function() {
-                    $scope.loading = false;
-                });
-
+            });
+            //add code here
 
         };
     });
